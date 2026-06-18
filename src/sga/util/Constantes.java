639344626b043e357190bdc9c24889bdeb0e4a8c
Yanis OUEDRAOGO <yanisouedraogo4@gmail.com>
2026-06-18 10:19:14 +0000

    Ajout du package Util (Connexion BD et utilitaires)

--- src/sga/util/Constantes.java
+++ src/sga/util/Constantes.java
@@ -1,11 +1,12 @@
+
 package sga.util;
 
 public class Constantes {
-    public static final String DB_URL = "jdbc:oracle:thin:@localhost:1521:XE"; 
-    
-    // Spécifier sys en tant que sysdba
-    public static final String DB_USER = "sys as sysdba"; 
-    
+    public static final String DB_URL = "jdbc:oracle:thin:@localhost:1521/XEPDB1";
+
+    // Spécifier le nom d'utilisateur
+    public static final String DB_USER = "sga_user";
+
     // Votre mot de passe de base de données
-    public static final String DB_PASS = "Conception2028";
+    public static final String DB_PASS = "Sga2026!";
 }
